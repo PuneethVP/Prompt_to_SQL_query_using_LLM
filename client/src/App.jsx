3bcf71f58ddd0a2066818deb2de048dcfa682c44
PuneethVP <puneethv08@gmail.com>
2023-12-22 18:15:43 -0600

    Final Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,47 @@
-import { useState , useRef } from 'react'
-import styles from './index.module.css'
-import sqlLogo from './assets/sql-logo.png'
+import { useState, useRef } from 'react';
+import styles from './index.module.css';
+import sqlLogo from './assets/sql-logo.png';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { RiFileCopyLine } from 'react-icons/ri';
 
 function App() {
-  const[queryDescription, setQueryDescription] = useState("");
-  const [sqlQuery, setSqlQuery] = useState("");
+  const [queryDescription, setQueryDescription] = useState('');
+  const [sqlQuery, setSqlQuery] = useState('');
   const codeRef = useRef(null);
-  const [isResizing, setIsResizing] = useState(false);
+  const [resizing, setResizing] = useState(false);
   const [width, setWidth] = useState(400); // Initial width of the code block
-  const codeBlockRef = useRef(null);
+  const [height, setHeight] = useState(200); // Initial height of the code block
 
   const handleMouseDown = (e) => {
-    setIsResizing(true);
+    setResizing(true);
   };
 
   const handleMouseMove = (e) => {
-    if (isResizing) {
-      setWidth(e.clientX - codeBlockRef.current.getBoundingClientRect().left);
+    if (resizing) {
+      const newWidth = e.clientX - codeRef.current.getBoundingClientRect().left;
+      const newHeight = e.clientY - codeRef.current.getBoundingClientRect().top;
+      setWidth(newWidth);
+      setHeight(newHeight);
     }
   };
 
   const handleMouseUp = () => {
-    setIsResizing(false);
+    setResizing(false);
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
     const generatedQuery = await generateQuery();
     setSqlQuery(generatedQuery);
-  }
+  };
 
   const copyToClipboard = () => {
-    if (codeBlockRef.current) {
-      const el = codeBlockRef.current.innerText;
+    if (codeRef.current) {
+      const el = codeRef.current.innerText;
       navigator.clipboard.writeText(el).then(() => {
-        // Success message or handle success
         console.log('Code copied to clipboard!');
       }).catch((error) => {
-        // Error handling if copy fails
         console.error('Unable to copy:', error);
       });
     }
@@ -57,47 +58,49 @@ function App() {
 
     const data = await response.json();
     return data.response.trim();
-  }
+  };
+
   return (
     <main className={styles.main}>
-      <img src={sqlLogo} alt="" className = {styles.icon} />
-      <h3>  Generate SQL with Prompt </h3>
-
+      <img src={sqlLogo} alt="" className={styles.icon} />
+      <h3>Generate SQL with Prompt</h3>
 
       <form onSubmit={onSubmit}>
-        <input type="text" 
-        name="query-description" 
-        placholder="prompt your query" 
-        onChange={(e)=> setQueryDescription(e.target.value)}
+        <input
+          type="text"
+          name="query-description"
+          placeholder="prompt your query"
+          onChange={(e) => setQueryDescription(e.target.value)}
         />
         <input type="submit" value="Generate SQL query" />
-        
       </form>
 
       <div style={{ borderRadius: '8px', backgroundColor: '#ffffff', padding: '10px' }}>
-          <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
-            <button onClick={copyToClipboard} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
-              <RiFileCopyLine style={{ fontSize: '1.5em' }} />
-            </button>
-          </div>
-          <div style={{ position: 'relative', maxWidth: '100%' }}>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
+          <button onClick={copyToClipboard} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+            <RiFileCopyLine style={{ fontSize: '1.5em' }} />
+          </button>
+        </div>
+        <div style={{ position: 'relative', maxWidth: '100%' }}>
           <SyntaxHighlighter
             language="javascript"
             style={vscDarkPlus}
             customStyle={{
               width: `${width}px`,
-              resize: 'horizontal',
+              height: `${height}px`,
+              resize: 'both',
               overflow: 'auto',
               borderRadius: '8px',
               border: '1px solid #ccc',
               padding: '10px',
               whiteSpace: 'pre-wrap',
             }}
-            ref={codeBlockRef}
+            ref={codeRef}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
           >
             {sqlQuery}
           </SyntaxHighlighter>
-
           <div
             style={{
               position: 'absolute',
@@ -105,16 +108,14 @@ function App() {
               bottom: '0',
               width: '10px',
               height: '10px',
-              cursor: 'col-resize',
+              cursor: 'se-resize',
             }}
             onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
           />
         </div>
-        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
